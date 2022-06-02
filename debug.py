a99f32bca73132188548948e9334f0657602312e
LukeG007 <luke@gilsinger.net>
2022-06-01 21:09:27 -0400

    Update mod

--- debug.py
+++ debug.py
@@ -2,7 +2,7 @@ name = 'Debug Mode'
 class Mod:
     def __init__(self, room_renderer, save_sys, persistant_save_sys, texture_sys):
         room_renderer.debug = True
-    def tick(self, room_renderer, save_sys, persistant_save_sys, texture_sys):
+    def tick(self):
         pass
-    def close(self, room_renderer, save_sys, persistant_save_sys, texture_sys):
+    def close(self):
         pass
